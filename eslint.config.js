547edf489c10336917cfb95cd50dccf2311d2de5
Ricardo Medina <medina.r.dev@gmail.com>
2024-12-05 16:10:33 -0400

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 import eslint from "@eslint/js"
 import ts_eslint from "typescript-eslint"
-import functional from "eslint-plugin-functional/flat"
+import functional from "eslint-plugin-functional"
 
 export default ts_eslint.config({
 	extends: [
@@ -10,7 +10,7 @@ export default ts_eslint.config({
 	],
 	plugins: {
 		"@typescript-eslint": ts_eslint.plugin,
-		functional
+		functional,
 	},
 	languageOptions: {
 		parserOptions: {
@@ -19,25 +19,42 @@ export default ts_eslint.config({
 		},
 	},
 	rules: {
-		"semi": ["error", "never"],
-		"quotes": ["error", "double", {
-			"allowTemplateLiterals": true
-		}],
+		semi: ["error", "never"],
+		indent: ["error", "tab"],
+		"no-trailing-spaces": "error",
+		"no-mixed-spaces-and-tabs": "error",
+		quotes: [
+			"error",
+			"double",
+			{
+				allowTemplateLiterals: true,
+			},
+		],
 		"no-console": "error",
 		"prefer-const": "off",
-		"curly": "error",
-		"@typescript-eslint/no-this-alias": ["error", {
-			"allowedNames": ["me"]
-		}],
+		curly: "error",
+		"@typescript-eslint/no-this-alias": [
+			"error",
+			{
+				allowedNames: ["me"],
+			},
+		],
 		"@typescript-eslint/await-thenable": "error",
 		"@typescript-eslint/ban-ts-comment": "off",
 		"@typescript-eslint/method-signature-style": ["error", "property"],
-		"functional/immutable-data": ["error", {
-			"ignoreClasses": true,
-			"ignoreAccessorPattern": "**.*_m*.**",
-			"ignoreNonConstDeclarations": {
-				"treatParametersAsConst": true
-			}
-		}],
+		"functional/immutable-data": [
+			"error",
+			{
+				ignoreClasses: true,
+				ignoreAccessorPattern: "**.*_m*.**",
+				ignoreNonConstDeclarations: {
+					treatParametersAsConst: true,
+				},
+			},
+		],
+		"@typescript-eslint/no-unused-vars": ["error", {
+			varsIgnorePattern: "^_",
+			argsIgnorePattern: "^_"
+		}]
 	}
 })
